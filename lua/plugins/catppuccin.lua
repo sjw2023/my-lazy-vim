5bec79585f38f6c13e01fa63daa13be4aa8d9b28
JoowonSuh <sjw2023@gmail.com>
2025-12-16 21:03:25 +0900

    mod: tabstep

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -3,6 +3,12 @@ return {
     "catppuccin/nvim",
     opts = {
       transparent_background = true,
+      custom_highlights = function(colors)
+        return {
+          LineNr = { fg = colors.overlay1 },
+          CursorLineNr = { fg = colors.lavender },
+        }
+      end,
       float = {
         transparent = true,
       },
